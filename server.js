4503d0763c44987d41d688b5a5693bf35c654c5e
Evan Turner <evturn@gmail.com>
2015-01-22 17:21:36 -0500

    express built for requests

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:3000/beerlocker');
 
 var app = express();
+var port = process.env.PORT || 3000;
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	res.json({message: 'Hi br0'});
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Listening on port ' + port);
