76f8cc204c87d4e9783985c63ea991a767faabdf
Evan Turner <evturn@gmail.com>
2015-01-23 03:46:05 -0500

    added user schema and password encryption pre to check before a user saves

--- models/user.js
+++ models/user.js
@@ -12,3 +12,18 @@ var UserSchema = new mongoose.Schema({
 		required: true
 	}
 });
+
+UserSchema.pre('save', function(callback) {
+	var user = this;
+	if (!user.isModified('password')) return callback();
+	bcrypt.genSalt(5, function(err, salt) {
+		if (err) return callback(err);
+		bcrypt.hash(user.password, salt, null, function(err, hash) {
+			if (err) return callback(err);
+			user.password = hash;
+			callback();
+		});
+	});
+});
+
+module.exports = mongoose.model('User', UserSchema);
