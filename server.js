c52f78f7a07c594c4a2df54b6a000ce6c5e77d6f
Evan Turner <evturn@gmail.com>
2015-01-23 11:09:39 -0500

    user controller and routes created inside server

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var beerController = require('./controllers/beer');
+var userController = require('./controllers/user');
 
 mongoose.connect('mongodb://localhost:27017/beerlocker')
-
 var app = express();
-
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
 var port = process.env.PORT || 3000;
 var router = express.Router();
+
+
+router.route('/users')
+	.post(userController.postUsers)
+	.get(userController.getUsers);
+
+
 
 router.route('/beers')
 	.post(beerController.postBeers)
