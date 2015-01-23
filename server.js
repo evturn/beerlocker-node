c912f826044c8c3f8b8dda6e9e112982fc3393c9
Evan Turner <evturn@gmail.com>
2015-01-23 11:49:52 -0500

    added authentication into endpoints for all routes except POST to user for creating a new user

--- server.js
+++ server.js
@@ -2,34 +2,39 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 var beerController = require('./controllers/beer');
 var userController = require('./controllers/user');
+var authController = require('.controllers/auth');
 
 mongoose.connect('mongodb://localhost:27017/beerlocker')
 var app = express();
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-var port = process.env.PORT || 3000;
+app.use(passport.initialize());
+
 var router = express.Router();
 
 
-router.route('/users')
-	.post(userController.postUsers)
-	.get(userController.getUsers);
-
-
-
 router.route('/beers')
-	.post(beerController.postBeers)
-	.get(beerController.getBeers);
+	.post(authController.isAuthenticated, beerController.postBeers)
+	.get(authController.isAuthenticated, beerController.getBeers);
 
 router.route('/beers/:beer_id')
-	.get(beerController.getBeer)
-	.put(beerController.putBeer)
-	.delete(beerController.deleteBeer);
+	.get(authController.isAuthenticated, beerController.getBeer)
+	.put(authController.isAuthenticated, beerController.putBeer)
+	.delete(authController.isAuthenticated, beerController.deleteBeer);
+
+router.route('/users')
+	.post(userController.postUsers)
+	.get(authController.isAuthenticated, userController.getUsers);
+
+
+
 
 app.use('/api', router);
 
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Listening on port ' + port);
