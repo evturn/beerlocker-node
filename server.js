4075cb525b85a3f9cbbedff12b22f7899c7d62e5
Evan Turner <evturn@gmail.com>
2015-01-23 03:35:18 -0500

    controllers now handling all beer related routes

--- server.js
+++ server.js
@@ -19,64 +19,10 @@ router.route('/beers')
 	.post(beerController.postBeers)
 	.get(beerController.getBeers);
 
-
-
-
-router.get('/', function(req, res) {
-	res.json({message: 'Stop looking at me'});
-});
-
-var beersRoute = router.route('/beers');
-
-beersRoute.post(function(req, res) {
-	var beer = new Beer();
-	beer.name = req.body.name;
-	beer.type = req.body.quantity;
-	beer.save(function(err) {
-		if (err)
-			res.send(err);
-		res.json({message: 'Beer added to the locker!', data: beer});
-	});
-});
-
-beersRoute.get(function(req, res) {
-	Beer.find(function(err, beers) {
-		if (err)
-			res.send(err);
-		res.json(beers);
-	});
-});
-
-var beerRoute = router.route('/beers/:beer_id');
-
-beerRoute.get(function(req, res) {
-	Beer.findById(req.params.beer_id, function(err, beer) {
-		if(err)
-			res.send(err);
-		res.json(beer);
-	});
-});
-
-beerRoute.put(function(req, res) {
-	Beer.findById(req.params.beer_id, function(err, beer) {
-		if (err)
-			res.send(err);
-		beer.quantity = req.body.quantity;
-		beer.save(function(err) {
-			if (err)
-				res.send(err)
-			res.json(beer);
-		});
-	});
-});
-
-beerRoute.delete(function(req, res) {
-	Beer.findByIdAndRemove(req.params.beer_id, function(err) {
-		if (err)
-			res.send(err);
-		res.json({message: 'Beer removed from the locker!'});
-	});
-});
+router.route('/beers/:beer_id')
+	.get(beerController.getBeer)
+	.put(beerController.putBeer)
+	.delete(beerController.deleteBeer);
 
 app.use('/api', router);
 
