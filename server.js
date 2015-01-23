126bdbe747705e01c1594e124b950fb321d22049
Evan Turner <evturn@gmail.com>
2015-01-22 19:02:57 -0500

    node debugger and other modules brought in

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
+
 var express = require('express');
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:3000/beerlocker');
-
 var app = express();
 var port = process.env.PORT || 3000;
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.json({message: 'Hi br0'});
+	res.json({message: 'Stop looking at me'});
 });
 
 app.use('/api', router);
